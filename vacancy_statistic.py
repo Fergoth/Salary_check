7a611edcae98875c60ae1f3f76b3e3fccbcc3bc9
fergoth <fergothew@gmail.com>
2024-11-08 18:56:07 +0300

    Упрщение создания словаря

--- vacancy_statistic.py
+++ vacancy_statistic.py
@@ -76,58 +76,52 @@ def predict_rub_salary_sj(vacancy):
 
 def get_all_vacancies_sj(lang, token):
     for page in count():
-        vacancy = get_sj_vacancies_by_language(lang, token, page)
-        yield vacancy
-        if not vacancy["more"]:
+        vacancies_page = get_sj_vacancies_by_language(lang, token, page)
+        yield vacancies_page
+        if not vacancies_page["more"]:
             break
 
 
 def get_stats_vacancies_sj(languages, token):
-    stat_all_languages_sj = {
-        lang: {
-            "vacancies_found": 0,
-            "vacancies_processed": 0,
-            "average_salary": 0
-        } for lang in languages
-    }
+    stat_all_languages_sj = {}
     for lang in languages:
+        average_salary = 0
         proceded_vacancies = []
         for vacancy_response in get_all_vacancies_sj(lang, token):
-            vacancy_page = vacancy_response["objects"]
-            stat_all_languages_sj[lang]["vacancies_found"] = vacancy_response["total"]
-            for vacancy in vacancy_page:
+            vacancies_page = vacancy_response["objects"]
+            for vacancy in vacancies_page:
                 predicted_salary = predict_rub_salary_sj(vacancy)
                 if predicted_salary:
                     proceded_vacancies.append(predicted_salary)
-        stat_all_languages_sj[lang]["vacancies_processed"] = len(proceded_vacancies)
         if len(proceded_vacancies):
-            stat_all_languages_sj[lang]["average_salary"] = sum(
-                proceded_vacancies
-            ) // len(proceded_vacancies)
+            average_salary = sum(proceded_vacancies) // len(proceded_vacancies)
+        stat_all_languages_sj[lang] = {
+            "vacancies_found": vacancy_response["total"],
+            "vacancies_processed": len(proceded_vacancies),
+            "average_salary": average_salary
+        }
     return stat_all_languages_sj
 
 
 def get_stats_vacancies_hh(languages):
-    stat_all_languages_hh = {
-        lang: {
-            "vacancies_found": 0,
-            "vacancies_processed": 0,
-            "average_salary": 0
-        } for lang in languages
-    }
+    stat_all_languages_hh = {}
     for lang in languages:
+        average_salary = 0
         proceded_vacancies = []
         vacancies, vacancies_count = fetch_vacancies_hh(lang)
-        stat_all_languages_hh[lang]["vacancies_found"] = vacancies_count
         for vacancy in vacancies:
             predicted_salary = predict_rub_salary_hh(vacancy)
             if predicted_salary:
                 proceded_vacancies.append(predicted_salary)
-        stat_all_languages_hh[lang]["vacancies_processed"] = len(proceded_vacancies)
         if len(proceded_vacancies):
-            stat_all_languages_hh[lang]["average_salary"] = sum(
+            average_salary = sum(
                 proceded_vacancies
             ) // len(proceded_vacancies)
+        stat_all_languages_hh[lang] = {
+            "vacancies_found": vacancies_count,
+            "vacancies_processed": len(proceded_vacancies),
+            "average_salary": average_salary
+        }
     return stat_all_languages_hh
 
 
